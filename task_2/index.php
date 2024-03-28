434e02f25a5a88bab9c1f6d680988e4e3b991245
ritik <ritikmaheshwari1203@gmail.com>
2024-03-28 13:48:09 +0530

    adding new feature, now user can update it,s own profile

---
+++ task_2/index.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+if(isset($_SESSION['start']) && $_SESSION['start'] ==true){
+    $loggin=true;
+}
+else{
+    header("location:login.php");
+}
+
+// echo $_SESSION['start'];
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">MG Tech  </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+
+      </ul>
+      <!-- <form class="d-flex" role="search"> -->
+        <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
+        <a href="./update_profile.php"><button class="btn btn-outline-success" type="button">Edit Profile</button></a> 
+      <!-- </form> -->
+
+    </div>
+  </div>
+</nav>
+
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner" style = "height:20rem; object-fit: contain;">
+    <div class="carousel-item active">
+      <img  style = "height:20rem; object-fit: cover;" src="pexels-sebastiaan-stam-1097456.jpg" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item">
+      <img   style = "height:20rem; object-fit: cover;"  src="pexels-sebastiaan-stam-1097456.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img  style = "height:20rem; object-fit: cover;"  src="pexels-sebastiaan-stam-1097456.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<div class="container">
+  <h1 class="">
+    welcome back <?php  echo $_SESSION['name']; ?>
+  </h1>
+
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ task_2/update_profile.php
@@ -0,0 +1,117 @@
+<?php
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+          }
+
+          $servername="localhost";
+          $username="root";
+          $database="auth_login";
+          $password="";
+          
+          $conn=mysqli_connect($servername, $username, $password, $database);
+
+
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        
+        $username=test_input($_POST['name']);
+        $user_password=test_input($_POST['password']);
+        $user_email=test_input($_POST['email']);
+
+        if($username==''||$user_password==''||$user_email==''){
+            echo '<div class="alert alert-danger" role="alert">
+                Please fill all the Entries!
+                </div>
+                ';
+            }
+
+        else{
+
+            $existtable="SELECT * FROM `signup` where email='$user_email'";
+            $result=mysqli_query($conn,$existtable);
+            $no_of_exist_rows= mysqli_num_rows($result);
+
+            if($no_of_exist_rows>0){
+                // echo '<div class="alert alert-danger" role="alert">
+                // Email is already exist please try another email!
+                // </div>
+                // ';
+                session_start();
+
+               $id =  $_SESSION['id'];
+
+                // $sql="INSERT INTO `signup` (`name`, `email`,`password`) VALUES ('$username', '$user_email', '$user_password')";
+                $sql="UPDATE `signup` SET `name`='$username', `email`='$user_email',`password`='$user_password' WHERE id = $id";
+                $result=mysqli_query($conn,$sql);
+                echo '<div class="alert alert-info" role="alert">
+                                            Success fully update the profile|
+                                    </div>';
+                                    // header("location:login.php");
+                
+            }
+
+            else{
+
+                // $sql="INSERT INTO `signup` (`name`, `email`,`password`) VALUES ('$username', '$user_email', '$user_password')";
+                //     $result=mysqli_query($conn,$sql);
+                //     echo '<div class="alert alert-info" role="alert">
+                //                                 Success fully Signup!
+                //                         </div>';
+                //                         header("location:login.php");
+
+            }
+        }
+
+        }
+
+
+
+?>
+
+<?php
+
+
+
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+        <div class= "container">
+        <form action = '<?php htmlspecialchars($_SERVER["PHP_SELF"])?>' method = "post">
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Enter Name here</label>
+                <input type="text" name = "name" class="form-control" id="" aria-describedby="emailHelp">
+                <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" name = "email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" name = "password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <!-- <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div> -->
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
