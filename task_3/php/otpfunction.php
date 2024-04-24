51816d4b3911b223a06fc0c0105a0bf509eeacb2
Ritik Maheshwari <109062018+ritikmaheshwari1203@users.noreply.github.com>
2024-04-24 15:52:01 +0530

    Update otpfunction.php

--- task_3/php/otpfunction.php
+++ task_3/php/otpfunction.php
@@ -30,7 +30,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'ritikmaheshwari1203@gmail.com';                     //SMTP username
-    $mail->Password   = 'ljzd bvey empd xjyt';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
